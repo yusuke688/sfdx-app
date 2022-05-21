00d035c02a10e4ccfc824aaa9b83c962561b0a20
yusuke <yusukem2gg@gmail.com>
2022-05-21 14:00:23 +0900

    LWC map in Account Record page

---
+++ force-app/main/default/lwc/lwc_SampleMap/lwc_SampleMap.js
@@ -0,0 +1,59 @@
+import { LightningElement, api, wire, track } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
+
+const FIELDS = [
+    'Account.Name'
+    , 'Account.BillingPostalCode'
+    , 'Account.BillingCountry'
+    , 'Account.BillingState'
+    , 'Account.BillingCity'
+    , 'Account.BillingStreet'
+    , 'Account.BillingLatitude'
+    , 'Account.BillingLongitude'
+];
+
+export default class Lwc_SampleMap extends LightningElement {
+    @track mapMarkers = [];
+
+    @api recordId;
+
+    @api zoomLevel;
+
+    @wire(getRecord, { recordId: '$recordId', fields: FIELDS})
+    wiredAccount({ data }) {
+        console.log('wiredAccount-0');
+        console.log(data);
+        if(data){
+            console.log('if-data');
+            let markers = [];
+            let description = data.fields.BillingPostalCode.value + '<br/>'
+            description += data.fields.BillingCountry.value + ' '
+            description += data.fields.BillingState.value + '<br/>'
+            description += data.fields.BillingCity.value + '<br/>'
+            description += data.fields.BillingStreet.value + '<br/>'
+            
+            
+            markers.push({
+                location: {
+                    Latitude: data.fields.BillingLatitude.value,
+                    Longitude: data.fields.BillingLongitude.value,
+                },
+                title: data.fields.Name.value,
+                description: description, 
+                icon: 'standard:account',
+            });
+            this.mapMarkers = markers;
+        }
+    }
+
+    onClickOpenGoogleMap(){
+        console.log('onClickOpenGoogleMap');
+
+        let url = 'https://maps.google.com/maps?q=';
+        url += this.mapMarkers[0].location.Latitude + ',';
+        url += this.mapMarkers[0].location.Longitude;
+        url += '&z=' + this.zoomLevel;
+
+        window.open(url);
+    }
+}
